7895a456c1940d563ce8a22a3a6156916850710f
Eugene Prokopiev <enp@itx.ru>
2010-12-31 10:19:23 +0300

    initial sessions persistence on register and unregister events

--- src/main/java/ru/itx/ccm/beans/EventListener.java
+++ src/main/java/ru/itx/ccm/beans/EventListener.java
@@ -15,25 +15,73 @@
  */
 package ru.itx.ccm.beans;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Node;
+import org.freeswitch.esl.client.IEslEventListener;
+import org.freeswitch.esl.client.inbound.Client;
+import org.freeswitch.esl.client.transport.event.EslEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class EventListener {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	private EventManager eventManager;
+	private Client client = new Client();
+
+	private String host;
+	private int port;
+	private String password;
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public void setEventManager(EventManager eventManager) {
 		this.eventManager = eventManager;
 	}
 
-	public void init() {
+	public void init() throws Exception {
 		logger.debug("init");
-		eventManager.connectSession("1", "2", "3", "4");
+		client.addEventListener(new IEslEventListener() {
+			public void eventReceived(EslEvent event) {
+				if (event.getEventName().equals("CUSTOM")) {
+					Map<String,String> headers = event.getEventHeaders();
+					String subclass = headers.get("Event-Subclass");
+					if (subclass.equals("sofia::register")) {
+						eventManager.connectSession(
+							headers.get("call-id"),
+							headers.get("from-user"),
+							headers.get("user-agent"),
+							headers.get("network-ip"));
+					} else if (subclass.equals("sofia::unregister")) {
+						eventManager.disconnectSession(headers.get("call-id"));
+					}
+				}
+			}
+			public void backgroundJobResultReceived(EslEvent event) {
+
+			}
+		});
+		client.connect(host, port, password, 2);
+		client.setEventSubscriptions("plain", "all");
 	}
 
 	public void destroy() {
-		eventManager.disconnectSession("1");
+		client.close();
 		logger.debug("destroy");
 	}
 }
